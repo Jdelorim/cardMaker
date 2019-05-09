fa414b0896a11b584363c471c25829481985e1a9
Joshua deLorimier <TRASHTRASH_HQ@Joshuas-Mac-Pro.localdomain>
2019-05-09 16:14:11 -0400

    heroku d

--- card-maker/server.js
+++ card-maker/server.js
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 if(process.env.NODE_ENV === 'production') {
-    app.use(express.static("client/build"));
+    app.use(express.static('client/build'));
 }
 
 app.listen(PORT,()=>{
