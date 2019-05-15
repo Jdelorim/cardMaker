922f4ae7ac427de26a484b838140f0f0e948adbe
Jdelorim <joshuadelorimier@gmail.com>
2019-05-15 16:00:00 -0400

    working on new machine

--- server.js
+++ server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
-const PORT = process.env.PORT|| 4001;
+const PORT = process.env.PORT|| 3001;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-if(process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
-}
+// if(process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
+// }
 app.get("*",(req,res)=>{
     res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 })
